c98f7e8f8e572f538c09f7c28ed3f503a7b37c1e
Henry-Hong <hhr8540@naver.com>
2023-04-24 17:19:00 +0900

    First Commit

---
+++ src/main/java/heroinie/com/springstudy/SpringStudyApplication.java
@@ -0,0 +1,13 @@
+package heroinie.com.springstudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringStudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringStudyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/heroinie/com/springstudy/SpringStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package heroinie.com.springstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringStudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
