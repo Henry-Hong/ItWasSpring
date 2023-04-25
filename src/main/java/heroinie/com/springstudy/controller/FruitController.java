d6219c5a9efeee773366e5937871d5fe9f722b2e
Henry-Hong <hhr8540@naver.com>
2023-04-26 07:51:45 +0900

    응답방법 3가지 (stataic HTML, template HTML, class JSON)

--- src/main/java/heroinie/com/springstudy/controller/FruitController.java
+++ src/main/java/heroinie/com/springstudy/controller/FruitController.java
@@ -3,6 +3,8 @@ package heroinie.com.springstudy.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class FruitController {
@@ -10,5 +12,43 @@ public class FruitController {
     public String fruit(Model model){
         model.addAttribute("favorite", "grape");
         return "fruit";
+    }
+
+    @GetMapping("/parameter")
+    public String param(@RequestParam(value = "name", required = false) String name, Model model){
+        model.addAttribute("name", name);
+        return "parameter";
+    }
+
+    @GetMapping("/body")
+    @ResponseBody
+    public String body(@RequestParam(value = "key", required = false) String key){
+        return "key is " + key;
+    }
+
+    @GetMapping("/api")
+    @ResponseBody
+    public CustomClass api(@RequestParam("name") String name){
+        CustomClass customClass = new CustomClass();
+        customClass.setName(name);
+        return customClass;
+    }
+
+    static class CustomClass {
+        private String name;
+        public String age;
+
+        public CustomClass() {
+            this.age = "1541";
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            System.out.print("zzzz"+name);
+            this.name = name;
+        }
     }
 }
